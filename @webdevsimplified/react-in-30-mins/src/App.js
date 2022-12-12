328dfc0b21c3837eaf4f29dfc960a6af0ae858d0
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-12-11 19:20:56 -0500

    [@wds] error from returning 2 elements in App

--- @webdevsimplified/react-in-30-mins/src/App.js
+++ @webdevsimplified/react-in-30-mins/src/App.js
@@ -3,7 +3,9 @@ import TodoList from './TodoList'
 
 export default function App() {
   // renders TodoList
+  // can only return one XML element
   return (
     <TodoList />
+    <input type='text' />
   );
 };
