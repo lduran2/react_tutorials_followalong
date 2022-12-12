6664188a61b419afc78751c35c02250d08efd499
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-12-11 22:24:18 -0500

    [@wds] added listener to Add button

--- @webdevsimplified/react-in-30-mins/src/App.js
+++ @webdevsimplified/react-in-30-mins/src/App.js
@@ -9,25 +9,13 @@ export default function App() {
     // for updating todos
     setTodos
   ] =
-    // default App state
-    // test with 2 Todos
-    useState(
-      [
-        {
-          "name": "Todo 1",
-          "complete": true
-        },
-        {
-          "name": "Todo 2",
-          "complete": false
-        },
-      ]
-      // auto increment the ID
-      .map((todo, i) => {
-          todo.id = i;
-          return todo;
-      })
-    )
+    // default App state to empty list
+    useState([])
+
+  function handleAddTodo(e) {
+    console.log(e);
+  }
+
   // renders TodoList
   // can only return one XML element
   // sending todos as a prop for TodoList
@@ -35,7 +23,7 @@ export default function App() {
     <>
       <TodoList todos={ todos } />
       <input type='text' />
-      <button>Add Todo</button>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear completed Todos</button>
       <div><span>0</span> left to do</div>
     </>
